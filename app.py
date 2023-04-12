b7be46760d32974e187417f57aa90584468f9315
SakshiMore <moresakshi228@gmail.com>
2023-04-13 01:30:09 +0530

    chatbot

--- app.py
+++ app.py
@@ -175,7 +175,7 @@ def generate_frames(name):
 app.secret_key = 'xyzsdfg'
   
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_USER'] = 'lroot'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'yogi'
   
